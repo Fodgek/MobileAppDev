f84aef8978b3b99200a371d318140c939a63d268
Fodgek <nikita.serebro@mail.ru>
2024-03-11 00:33:54 +1000

    Создал пробную страничку соц сети, вышло так себе, но что есть

--- lib/main.dart
+++ lib/main.dart
@@ -13,25 +13,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Stein Gate Lovers'),
     );
   }
 }
@@ -39,14 +24,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -56,63 +33,120 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  bool value = false;
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
+    });
+  }
+  void _incrementCounter2() {
+    setState(() {
+      _counter = _counter + 2;
+    });
+  }
+  void _incrementCounter3() {
+    setState(() {
+      _counter = _counter + 3;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+         child: Padding(
+          padding: EdgeInsets.all(40),
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 70,
+                backgroundColor: Colors.blue,
+                child: Padding(
+                  padding: const EdgeInsets.all(8), // Border radius
+                  child:
+                  ClipOval(
+                      child:Image.asset (
+                        "assets/gif/test.gif")
+                  ),
+                ),
+              ),
+              Container(
+                  color: Colors.amber,
+                  padding: EdgeInsets.all(10),
+                  margin:  EdgeInsets.all(10),
+                  child:
+                  const Text("Серебреницкий Никита Антонович",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.white,
+                          fontSize: 26
+                      )
+                  )
+              ),
+              Container(
+                  color: Colors.blueAccent,
+                  padding: EdgeInsets.all(10),
+                  margin:  EdgeInsets.all(10),
+                  child:
+                  const Text("я не знаю какой функционал встваить,будет вот такой:",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 26
+                      )
+                  )
+              ),
+              OverflowBar(
+                alignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  TextButton( child: const Text('Button +1'), onPressed: _incrementCounter),
+                  TextButton( child: const Text('Button +2'), onPressed: _incrementCounter2),
+                  TextButton( child: const Text('Button +3'), onPressed: _incrementCounter3),
+                ],
+              ),
+              OverflowBar(
+                alignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Checkbox(
+                    value: this.value,
+                    onChanged: (bool? value) {setState(() {this.value = value!;});},
+                  ),
+                  Checkbox(
+                    value: this.value,
+                    onChanged: (bool? value) {setState(() {this.value = value!;});},
+                  ),
+                  Checkbox(
+                    value: this.value,
+                    onChanged: (bool? value) {setState(() {this.value = value!;});},
+                  ),
+                ],
+              ),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10),
+                margin:  EdgeInsets.all(10),
+                child:
+                Text("$_counter",
+                    style: const TextStyle(color: Colors.green,fontSize: 26,)
+                ),
+              ),
+              Container(
+                  color: Colors.white,
+                  padding: EdgeInsets.all(10),
+                  margin:  EdgeInsets.all(10),
+                  child:
+                  Text("$value",
+                      style: const TextStyle(color: Colors.red,fontSize: 26,)
+                  )
+              )
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
